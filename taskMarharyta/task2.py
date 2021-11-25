e251e2ede3386fdf3e408556b496c54e4bd974c5
Siarhei_Papina <Siarhei_papina@epam.com>
2021-11-25 16:11:02 +0300

    begin of project

---
+++ taskMarharyta/task2.py
@@ -0,0 +1,15 @@
+'''
+Task2:
+ Implement a function `get_pairs(lst: List) -> List[Tuple]`
+ which returns a list of tuples containing pairs of elements.
+ Pairs should be formed as in the example.
+ If there is only one element in the list return `None` instead.
+
+Example:
+ get_pairs([1, 2, 3, 8, 9])
+ [(1, 2), (2, 3), (3, 8), (8, 9)]
+ get_pairs(['need', 'to', 'sleep', 'more'])
+ [('need', 'to'), ('to', 'sleep'), ('sleep', 'more')]
+ get_pairs([1])
+ None
+'''
---
+++ taskMarharyta/task3.py
@@ -0,0 +1,12 @@
+'''
+Task3:
+ Implement a function,
+ that takes string as an argument and returns a dictionary,
+ that contains letters of given string as keys and a number of their occurrence as values.
+
+Example:
+ print(count_letters("stringsample"))
+ {'s': 2, 't': 1, 'r': 1, 'i': 1,
+ 'n': 1, 'g': 1, 'a': 1, 'm': 1,
+ 'p': 1, 'l': 1, 'e': 1}
+'''
